259cbec2e8b6498bb9fbb9f44ce5bbf0dcad8957
Roman <sankovich.roman@mail.ru>
2022-05-08 23:20:08 +0300

    Error

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import { Component } from "react/cjs/react.production.min";
+
+import ErrorMessage from "../errorMessage/errorMessage";
+
+class ErrorBoundary extends Component {
+  state = {
+    error: false
+  }
+
+  // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+  componentDidCatch(error, info) {
+    console.log(error, info);
+    this.setState({
+      error: true
+    })
+  }
+
+
+  render() {
+    if (this.state.error) {
+      return <ErrorMessage/>
+    }
+
+    return this.props.children;
+    
+  }
+
+}
+
+export default ErrorBoundary;
